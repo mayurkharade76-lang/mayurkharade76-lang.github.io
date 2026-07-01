7d0d9cad5bb814421513654ac0fb339d8835b2d9
mayurkharade76-lang <mayurkharade76@gmail.com>
2026-07-01 19:41:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+/* Theme Toggle */
+
+const themeBtn = document.getElementById("theme-toggle");
+
+if(themeBtn){
+
+themeBtn.addEventListener("click",()=>{
+
+document.body.classList.toggle("light-mode");
+
+const icon = themeBtn.querySelector("i");
+
+if(document.body.classList.contains("light-mode")){
+
+icon.classList.remove("fa-moon");
+icon.classList.add("fa-sun");
+
+}else{
+
+icon.classList.remove("fa-sun");
+icon.classList.add("fa-moon");
+
+}
+
+});
+
+}
 /* ==========================================
    AOS Animation
 ========================================== */
